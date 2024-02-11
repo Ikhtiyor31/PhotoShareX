4bcf37b32532500efa79c852921db2b549bf15c3
Ikhtiyor <55629713+Ikhtiyor31@users.noreply.github.com>
2024-02-12 01:46:07 +0900

    [Test] added test for update photo detail Repository

--- src/test/java/com/ikhtiyor/photosharex/photo/repository/PhotoRepositoryTest.java
+++ src/test/java/com/ikhtiyor/photosharex/photo/repository/PhotoRepositoryTest.java
@@ -2,7 +2,9 @@ package com.ikhtiyor.photosharex.photo.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.ikhtiyor.photosharex.exception.ResourceNotFoundException;
 import com.ikhtiyor.photosharex.photo.dto.PhotoRequest;
+import com.ikhtiyor.photosharex.photo.dto.PhotoUpdateRequest;
 import com.ikhtiyor.photosharex.photo.enums.VisibilityType;
 import com.ikhtiyor.photosharex.photo.model.Photo;
 import com.ikhtiyor.photosharex.user.dto.UserRegisterRequest;
@@ -36,7 +38,7 @@ class PhotoRepositoryTest {
         PhotoRequest photoRequest = new PhotoRequest(
             "http://localhost:8080/image_2024_02_11_23423.jpg",
             "New Image",
-            "This is a beatiful image",
+            "This is a beautiful image",
             VisibilityType.PUBLIC,
             "Seoul"
         );
@@ -44,6 +46,48 @@ class PhotoRepositoryTest {
         Photo savedPhoto = photoRepository.save(photo);
         assertThat(savedPhoto.getVisibilityType()).isEqualTo(VisibilityType.PUBLIC);
         assertThat(savedPhoto.getUser().getEmail()).isEqualTo("test@gmail.com");
+    }
+
+    @Test
+    void givenPhotoEntity_whenUpdatedPhoto_thenSuccess() {
+
+        UserRegisterRequest registerRequest = new UserRegisterRequest(
+            "abdul",
+            "test@gmail.com",
+            "aslfasjffasfd",
+            ""
+        );
+
+        User user = User.createOf(registerRequest, "encoasdfafalas");
+        User savedUser = userRepository.save(user);
+        PhotoRequest photoRequest = new PhotoRequest(
+            "http://localhost:8080/image_2024_02_11_23423.jpg",
+            "My old image",
+            "This is a beautiful old image",
+            VisibilityType.PUBLIC,
+            "Seoul"
+        );
+        Photo photo = Photo.createOf(photoRequest, savedUser);
+        Photo savedPhoto = photoRepository.save(photo);
+
+        Photo fetchedPhoto = photoRepository.findById(savedPhoto.getId()).orElseThrow(
+            () -> new ResourceNotFoundException("Photo not found with id: " + savedPhoto.getId()));
+
+        PhotoUpdateRequest photoUpdateRequest = new PhotoUpdateRequest(
+            "My new image",
+            "This is a beautiful new image",
+            "Seongnam-si"
+        );
+
+        fetchedPhoto.setTitle(photoUpdateRequest.title());
+        fetchedPhoto.setDescription(photoUpdateRequest.description());
+        fetchedPhoto.setLocation(photoUpdateRequest.location());
+
+        // Then
+        assertThat(fetchedPhoto.getLocation()).isEqualTo(photoUpdateRequest.location());
+        assertThat(fetchedPhoto.getLocation()).isNotEqualTo(photoRequest.location());
+        assertThat(fetchedPhoto.getTitle()).isNotEqualTo(photoRequest.title());
+
     }
 
 }
