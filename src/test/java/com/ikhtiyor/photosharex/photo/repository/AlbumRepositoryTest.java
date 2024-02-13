0aa5e4f80384984327ed48caad731649cb7d6a78
Ikhtiyor <55629713+Ikhtiyor31@users.noreply.github.com>
2024-02-14 01:51:36 +0900

    [Test] implement get my albums test for AlbumRepositoryTest

--- src/test/java/com/ikhtiyor/photosharex/photo/repository/AlbumRepositoryTest.java
+++ src/test/java/com/ikhtiyor/photosharex/photo/repository/AlbumRepositoryTest.java
@@ -2,17 +2,22 @@ package com.ikhtiyor.photosharex.photo.repository;
 
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.ikhtiyor.photosharex.photo.dto.AlbumRequest;
 import com.ikhtiyor.photosharex.photo.model.Album;
 import com.ikhtiyor.photosharex.user.dto.UserRegisterRequest;
 import com.ikhtiyor.photosharex.user.model.User;
 import com.ikhtiyor.photosharex.user.repository.UserRepository;
+import jakarta.persistence.EntityManager;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 @DataJpaTest
 class AlbumRepositoryTest {
@@ -23,9 +28,14 @@ class AlbumRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private EntityManager entityManager;
+
     @BeforeEach
     void setUp() {
         albumRepository.deleteAll();
+        userRepository.deleteAll();
+        entityManager.clear();
     }
 
     @Test
@@ -89,6 +99,52 @@ class AlbumRepositoryTest {
         final var savedAlbum = albumRepository.save(album);
         final var fetchAlbum = albumRepository.findByUserAndId(user2, savedAlbum.getId());
         assertThat(fetchAlbum).isEqualTo(Optional.empty());
+    }
+
+    @Test
+    void givenAlbum_whenFindByUser_thenReturnsOnlyMyAlbums() {
+        // Given
+        AlbumRequest albumRequest = new AlbumRequest(
+            "My new album",
+            "this is my description about album"
+        );
+        String email1 = "test1@gmail.com";
+        String email2 = "test2@gmail.com";
+        User user1 = getUser(email1);
+        User user2 = getUser(email2);
+
+        Album album1 = Album.of(albumRequest, user1);
+        Album album2 = Album.of(albumRequest, user2);
+        albumRepository.save(album1);
+        albumRepository.save(album2);
+
+        PageRequest pageable = PageRequest.of(0, 10, Sort.Direction.DESC, "createdAt");
+        final var fetchAlbum = albumRepository.findByUser(user1, pageable);
+        assertFalse(fetchAlbum.isEmpty());
+        assertThat(fetchAlbum.getTotalElements()).isEqualTo(1);
+    }
+
+    @Test
+    void givenAlbum_whenFindByUser_thenReturnsNull() {
+        // Given
+        AlbumRequest albumRequest = new AlbumRequest(
+            "My new album",
+            "this is my description about album"
+        );
+        String email1 = "test1@gmail.com";
+        String email2 = "test2@gmail.com";
+        User user1 = getUser(email1);
+        User user2 = getUser(email2);
+
+        Album album1 = Album.of(albumRequest, user1);
+        Album album2 = Album.of(albumRequest, user1);
+        albumRepository.save(album1);
+        albumRepository.save(album2);
+
+        PageRequest pageable = PageRequest.of(0, 10, Sort.Direction.DESC, "createdAt");
+        final var fetchAlbum = albumRepository.findByUser(user2, pageable);
+        assertTrue(fetchAlbum.isEmpty());
+        assertThat(fetchAlbum.getTotalElements()).isEqualTo(0);
     }
 
 
