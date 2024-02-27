7f810753459f62d8e44142dcd3627a8e3e0bea3e
Ikhtiyor <55629713+Ikhtiyor31@users.noreply.github.com>
2024-02-27 22:36:58 +0900

    [Test]: implement remove like LikeRepositoryTest

--- src/test/java/com/ikhtiyor/photosharex/like/repository/LikeRepositoryTest.java
+++ src/test/java/com/ikhtiyor/photosharex/like/repository/LikeRepositoryTest.java
@@ -3,7 +3,6 @@ package com.ikhtiyor.photosharex.like.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.ikhtiyor.photosharex.comment.model.Comment;
 import com.ikhtiyor.photosharex.like.model.Like;
 import com.ikhtiyor.photosharex.photo.dto.PhotoRequest;
 import com.ikhtiyor.photosharex.photo.enums.VisibilityType;
@@ -13,6 +12,7 @@ import com.ikhtiyor.photosharex.user.dto.UserRegisterRequest;
 import com.ikhtiyor.photosharex.user.model.User;
 import com.ikhtiyor.photosharex.user.repository.UserRepository;
 import jakarta.persistence.EntityManager;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -65,6 +65,33 @@ class LikeRepositoryTest {
         assertThat(like.getId()).isGreaterThan(0);
         assertThat(like.getId()).isEqualTo(1);
         assertThat(like.getUser().getEmail()).isEqualTo(email);
+    }
+
+    @Test
+    void givenLikeEntity_whenLikeRemoved_thenSuccess() {
+        // Given
+        PhotoRequest photoRequest = new PhotoRequest(
+            "http://localhost:8080/image_2024_02_11_23423.jpg",
+            "New Image",
+            "This is a beautiful image",
+            VisibilityType.PUBLIC,
+            "Seoul"
+        );
+        String email = "test1@gmail.com";
+        User user = getUser(email);
+        Photo photo = Photo.createOf(photoRequest, user);
+        Photo savedPhoto = photoRepository.save(photo);
+        Like like = Like.createOf(savedPhoto, user);
+        Like like1 = Like.createOf(savedPhoto, user);
+        Like like2 = Like.createOf(savedPhoto, user);
+
+        // When
+        likeRepository.saveAll(List.of(like, like1, like2));
+        like.setDeleted();
+        boolean likeExists = likeRepository.existsById(like.getId());
+
+        // Then
+        assertFalse(likeExists);
     }
 
     private User getUser(String email) {
