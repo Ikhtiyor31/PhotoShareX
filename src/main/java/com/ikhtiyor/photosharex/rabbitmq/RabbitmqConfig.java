d1fa405cd07f4f8040ae68174609bf1c1a327540
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-06-23 21:54:56 +0900

    added retry operations to RabbitmqConfig (#2)

--- src/main/java/com/ikhtiyor/photosharex/rabbitmq/RabbitmqConfig.java
+++ src/main/java/com/ikhtiyor/photosharex/rabbitmq/RabbitmqConfig.java
@@ -4,12 +4,14 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.interceptor.RetryOperationsInterceptor;
 
 @Configuration
 public class RabbitmqConfig {
@@ -23,12 +25,12 @@ public class RabbitmqConfig {
 
     @Bean
     Queue emailQueue() {
-        return new Queue(emailQueue, false);
+        return new Queue(emailQueue, true);
     }
 
     @Bean
     Queue fcmQueue() {
-        return new Queue(fmcQueue, false);
+        return new Queue(fmcQueue, true);
     }
 
     @Bean
@@ -56,5 +58,12 @@ public class RabbitmqConfig {
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RetryOperationsInterceptor retryInterceptor(){
+        return RetryInterceptorBuilder.stateless().maxAttempts(3)
+            .backOffOptions(2000, 2.0, 100000)
+            .build();
     }
 }
